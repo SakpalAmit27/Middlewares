1ba87229da9c75f8e00de73122f4047041b1bd93
Sakpal Amit <amitsakpal744@gmail.com>
2025-03-03 14:33:22 +0530

    [ demo ]
     - a demo code of accepting middlewares while utilizing next func param , now the middleware can be used in any endpoint targetting the specific user or id auth's

--- server.js
+++ server.js
@@ -10,3 +10,30 @@ app.listen(4000);
 app.get('/',(req,res) => {
     res.send(`<h2>Dashboard</h2>`)
 })
+
+
+
+const userMiddleware = (req,res,next) => {
+    if(user !== "amit" && pass !== "admin"){
+        res.status(403).json({message : "incorrect input"})
+    }else{
+        next(); // moving to the endpoint (exec the middleware)
+    }
+}
+
+
+const idMiddleware = (req,res,next) => {
+
+    if(!id == 1 || !id == 2){
+        res.status(403).json({message : "incorrect id"})
+    }else{
+        next();
+    }
+
+}
+
+
+app.get('/store', userMiddleware , idMiddleware , (req,res) => {
+
+    res.send("store endpoint")
+})
